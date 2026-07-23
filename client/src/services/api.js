9f29108213911b8d5626811b501163c4e80214eb
Rudra-Parsaniya <parsaniyarudra0018@gmail.com>
2026-07-23 19:53:14 +0530

    final ka final

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://job-application-website-h8sy.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
